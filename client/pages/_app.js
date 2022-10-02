768a469c218cca740255b4866931fd14b9726a50
Weave-Trix <88459877+Weave-Trix@users.noreply.github.com>
2022-10-02 08:24:36 +0800

    Client Critical Checkpoint : npm broken

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,72 @@
+import styled, { createGlobalStyle } from "styled-components";
+import { Colors } from "../src/Theme";
+import { useState } from "react";
+import Header from "../src/components/Header";
+
+const GlobalStyle = createGlobalStyle`
+ 
+  html,
+  body {
+    background-color: ${Colors.Background};
+    font-family: "Poppins", sans-serif;
+  }
+  p,a,h1,h2,h3,h5,h6,div,span{
+    /* color:${Colors.White}; */
+    color: inherit;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    transition:all .3s;
+    font-family: 'Urbanist', sans-serif;
+  }
+  /* width */
+  body::-webkit-scrollbar {
+    width: 5px;
+  }
+  /* Track */
+  body::-webkit-scrollbar-track {
+    background: #ffffff;
+  }
+  /* Handle */
+  body::-webkit-scrollbar-thumb {
+    background: #212121;
+    border-radius: 20px;
+  }
+  /* Handle on hover */
+  body::-webkit-scrollbar-thumb:hover {
+    background: rgb(43, 43, 43);
+  }
+`;
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  min-height: 100vh;
+`;
+const MobileMenu = styled.div``;
+const NavItem = styled.a``;
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [MobileMenuIsOpen, setMobileMenuIsOpen] = useState(false);
+  return (
+    <>
+      <GlobalStyle />
+      <Main>
+        <Header mobileMenu={{ MobileMenuIsOpen, setMobileMenuIsOpen }} />
+        {/* header */}
+        {/* page */}
+        {/* footer */}
+        <Component {...pageProps} />
+      </Main>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ client/src/components/Header/SearchBar.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+import { CgSearch } from "react-icons/cg";
+import { Colors, Devices } from "../../Theme";
+
+const SearchBarEl = styled.article`
+  background-color: ${Colors.Background};
+  padding: 0rem 1rem;
+  gap: 0.5rem;
+  height: 1;
+  max-width: 380px;
+  flex: 1;
+  align-items: center;
+  display: none;
+  @media ${Devices.Laptop} {
+    display: flex;
+  }
+`;
+
+const SearchBarBg = styled.div`
+  background-color: ${Colors.White};
+  display: flex;
+  width: 100%;
+  align-items: center;
+  gap: 0.5rem;
+  border-radius: 20px;
+  padding: 0.4rem 0.7rem;
+  svg {
+    font-size: 1.5rem;
+    color: ${Colors.Primary};
+  }
+`;
+
+const SearchInput = styled.input`
+  border: none;
+  font-size: 1.15rem;
+  flex: 1;
+  :focus {
+    outline: none;
+  }
+`;
+
+export default function SearchBar() {
+  return (
+    <SearchBarEl>
+    <SearchBarBg>
+      <CgSearch />
+      <SearchInput placeholder="Search vehicles" />
+    </SearchBarBg>
+  </SearchBarEl>
+  );
+}
